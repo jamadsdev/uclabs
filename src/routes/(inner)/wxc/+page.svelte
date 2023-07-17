4c74f532a5dab866c3093d1ce27ea37ad68cb60c
Adam Thompson <adam@uclabs.dev>
2023-07-16 21:22:54 -0400

    wxc-added token logic

--- src/routes/(inner)/wxc/+page.svelte
+++ src/routes/(inner)/wxc/+page.svelte
@@ -1,8 +1,32 @@
 <script>
 	/** @type {import('./$types').PageData} */
-	import { storeToken } from '$lib/stores.js';
+	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+	import { 
+	// Stores
+	import { storeWxcToken, storeWxcTab } from '$lib/stores.js';
+	// Components
+	import WxcVmPin from '$lib/components/WxcVMPin.svelte';
 
 	export let data;
 </script>
 
-Access Token: {$storeToken}
+Access Token: {$storeWxcToken}
+{#if $storeWxcTab == ''}
+	Need to enter token
+{:else}
+	<TabGroup>
+		<Tab bind:group={$storeWxcTab} name="tab1" value={0}>VM PIN Reset</Tab>
+		<Tab bind:group={$storeWxcTab} name="tab2" value={1}>(label 2)</Tab>
+		<Tab bind:group={$storeWxcTab} name="tab3" value={2}>Bulk Tasks</Tab>
+		<!-- Tab Panels --->
+		<svelte:fragment slot="panel">
+			{#if $storeWxcTab === 0}
+				<WxcVmPin />
+			{:else if $storeWxcTab === 1}
+				(tab panel 2 contents)
+			{:else if $storeWxcTab === 2}
+				(tab panel 3 contents)
+			{/if}
+		</svelte:fragment>
+	</TabGroup>
+{/if}
